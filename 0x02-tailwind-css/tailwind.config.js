79a5003917372e6dab0a41f99e81360a9b78251a
jaymoh001-gif <jamesmburu388@gmail.com>
2025-11-02 10:49:35 +0300

    Setup Tailwind CSS with configuration and build output

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -7,3 +7,12 @@ module.exports = {
   plugins: [],
 }
 
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
